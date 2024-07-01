35d88ce303e45187546c10b752b54a570afe039d
Aqib1422 <mraqibshafique@gmail.com>
2024-07-01 23:03:11 +0500

    Complete Complex Todo-List Object State

--- src/ObStateComplexTodo/ComplexTodo.jsx
+++ src/ObStateComplexTodo/ComplexTodo.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useState } from 'react'
 import toast from 'react-hot-toast'
+import TodoData from './TodoData'
 
 const ComplexTodo = () => {
     const [inputdata,setInputData] = useState([])
@@ -31,23 +32,37 @@ const ComplexTodo = () => {
                 when: ""
             })
         }
+    }
+    const removeTodo = (id) =>
+    {
+        toast.success('Task Removed Successfully')
+        setInputData(inputdata.filter((e, i) => i !== id))
         }
   return (
       <>
           <div className="container">
               <div className="col-lg-5 mx-auto p-3 rounded-5 shadow">
-                  <h4 className='text-center'>
+                  <h2 className='text-white text-center'>
                     Complex-Todo Object States
-                  </h4>
+                  </h2>
                   <form action="">
                       <label htmlFor="">Work</label>
-                      <input name='work' onChange={inputHandle} value={work} type="text" className='form-control' />
+                      <input name='work' onChange={inputHandle} value={work} type="text" className='my-3 form-control' />
                       <label htmlFor="">Date</label>
                       <input name='when' onChange={inputHandle} value={when} type="date" className='form-control' />
                       <button onClick={handleChange} className="btn my-2 btn-success w-50 d-block mx-auto">
                           Add Todo
                       </button>
                   </form>
+              </div>
+              <div className="rounded-5  my-3 col-lg-6 d-flex flex-column mx-auto p-3 shadow rounded-2">
+                  <h1 className='text-center yt'>Your Todo</h1>
+                  {inputdata?.map((items, index) =>
+                  {
+                      return <TodoData key={index}
+                          {...items} index={index} remove={removeTodo} 
+                      />
+                })}
               </div>
           </div>
       </>
---
+++ src/ObStateComplexTodo/TodoData.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react'
+import toast from 'react-hot-toast';
+import { MdDeleteForever } from "react-icons/md";
+const TodoData = ({ when, work, index, remove }) => {
+  const [done, setDone] = useState(false)
+  const handleClick= () =>
+  {
+    if (done == false) {
+      toast.success("You Done this Task")
+    }
+    else {
+      toast.error("You Canceled this Task")
+    }
+    
+    }
+  return (
+      <>
+          <div className="card my-2 rounded-4 ">
+              <div className="d-flex flex-column">
+                  <div className="rounded-4 text-white bg-dark card-header align-items-center justify-content-between d-flex flex-row">
+            <h3 className="mb-0" style={{
+              textDecoration: `${done? 'line-through':'none'}`
+            }}>{work}</h3>
+            <input onClick={handleClick} onChange={()=>setDone(!done)} type="checkbox"  />
+                  </div>
+                  <div className="text-white card-body d-flex flex-row align-items-center justify-content-between ">
+            <p className="mb-0" style={{
+              textDecoration: `${done? 'line-through':'none'}`
+            }}>{when}</p>
+            
+              <MdDeleteForever  onClick={()=>remove(index)}  size={25}  />
+             
+           
+              </div>
+              </div>
+       </div>
+      </>
+  )
+}
+
+export default TodoData
